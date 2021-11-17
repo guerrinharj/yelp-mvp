7d5594588f6ad381fca06d5f26bcb563e5b0d4fa
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-11-17 11:59:09 -0300

    Seeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+puts 'Seeding...'
+
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save!
+end
+
+10.times do
+  review = Review.new(
+    restaurant_id: rand(Restaurant.first.id...Restaurant.last.id),
+    rating: rand(1...5),
+    content: Faker::Restaurant.review
+  )
+  review.save!
+  puts "#{review.id}: #{review.restaurant}"
+end
